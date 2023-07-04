a83181034fedaca06289cb83450c92ef5d8e3832
Sofia-Ortega <s.ortega@tamu.edu>
2023-07-03 20:45:28 -0500

    another small change

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>PLEEEEEEEEEEEEEAAAAAAAAAASSSSSSSSSSEEEEEEEEEEE</p>
+        <p>ANd another change</p>
         <p>
           Edit <code>src/App.js</code> and commit to reload.
         </p>
